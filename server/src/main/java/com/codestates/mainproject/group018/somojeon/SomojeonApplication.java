a0618fc08b0986a12175c5a777dc2a8585880ec3
gnswp21 <gnswp21@gmail.com>
2023-03-03 16:36:31 +0900

    ✨ feat server init

---
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/SomojeonApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.mainproject.group018.somojeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomojeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomojeonApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codestates/mainproject/group018/somojeon/SomojeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.mainproject.group018.somojeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomojeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
