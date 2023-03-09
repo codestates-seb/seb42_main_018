3d2e6259b226366be41cd840ccd41c239f11e2fb
taekie <euntaek0813@naver.com>
2023-03-09 21:52:56 +0900

    ✨ feat : Comment 구현

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/comment/controller/CommentController.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/comment/controller/CommentController.java
@@ -1,31 +1,86 @@
 package com.codestates.mainproject.group018.somojeon.comment.controller;
 
+import com.codestates.mainproject.group018.somojeon.comment.dto.CommentDto;
+import com.codestates.mainproject.group018.somojeon.comment.entity.Comment;
+import com.codestates.mainproject.group018.somojeon.comment.mapper.CommentMapper;
+import com.codestates.mainproject.group018.somojeon.comment.service.CommentService;
+import com.codestates.mainproject.group018.somojeon.dto.MultiResponseDto;
+import com.codestates.mainproject.group018.somojeon.dto.SingleResponseDto;
+import com.codestates.mainproject.group018.somojeon.utils.UriCreator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
-@RestController
-@RequestMapping("/comments")
-public class CommentController {
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
+import java.util.List;
 
-    @PostMapping
-    public ResponseEntity postComment() {
-        return null;
+@Slf4j
+@Validated
+@RestController
+@RequestMapping
+public class CommentController {
+    private final CommentService commentService;
+    private final CommentMapper commentMapper;
+
+    public CommentController(CommentService commentService, CommentMapper commentMapper) {
+        this.commentService = commentService;
+        this.commentMapper = commentMapper;
     }
 
-    @PatchMapping
-    public ResponseEntity patchComment() {
-        return null;
+    @PostMapping("/records/{record-id}/comments")
+    public ResponseEntity postComment(@PathVariable("record-id") @Positive long recordId,
+                                      @Valid @RequestBody CommentDto.Post requestBody) {
+        requestBody.addRecordId(recordId);
+
+        Comment comment = commentMapper.commentPostDtoToComment(requestBody);
+
+        Comment createdComment = commentService.createComment(comment, recordId);
+        URI location = UriCreator.createUri("/records/{record-id}/comments", createdComment.getCommentId());
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/records/{record-id}/comments/{comment-id}")
+    public ResponseEntity patchComment(@PathVariable("record-id") @Positive long recordId,
+                                       @PathVariable("comment-id") @Positive long commentId,
+                                       @Valid @RequestBody CommentDto.Patch requestBody) {
+        requestBody.addRecordId(recordId);
+        requestBody.addCommentId(commentId);
+
+        Comment comment = commentService.updateComment(commentMapper.commentPatchDtoToComment(requestBody));
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(commentMapper.commentToCommentResponseDto(comment)), HttpStatus.OK);
+    }
+
+    @GetMapping("/comments/{comment-id}")
+    public ResponseEntity getComment(@PathVariable("comment-id") @Positive long commentId) {
+        Comment comment = commentService.findComment(commentId);
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(commentMapper.commentToCommentResponseDto(comment)), HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getComment() {
-        return null;
+    public ResponseEntity getComments(@RequestParam("page") int page,
+                                      @RequestParam("size") int size) {
+        Page<Comment> pageComments = commentService.findComments(page - 1, size);
+        List<Comment> comments = pageComments.getContent();
+
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(commentMapper.commentsToCommentResponseDtos(comments), pageComments),
+                HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public ResponseEntity deleteComment() {
-        return null;
+    @DeleteMapping("/comments/{comment-id}")
+    public ResponseEntity deleteComment(@PathVariable("comment-id") @Positive long commentId) {
+        commentService.deleteComment(commentId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- server/src/main/java/com/codestates/mainproject/group018/somojeon/comment/repository/CommentRepository.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/comment/repository/CommentRepository.java
@@ -3,5 +3,5 @@ package com.codestates.mainproject.group018.somojeon.comment.repository;
 import com.codestates.mainproject.group018.somojeon.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRespository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
