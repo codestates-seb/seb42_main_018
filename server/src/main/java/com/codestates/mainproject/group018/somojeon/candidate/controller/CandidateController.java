6a9e0f6f2c79ad0c073927f59fb5e83739bbf03c
taekie <euntaek0813@naver.com>
2023-03-09 11:46:57 +0900

    ✨ feat : annotation add

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/candidate/controller/CandidateController.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/candidate/controller/CandidateController.java
@@ -1,11 +1,12 @@
 package com.codestates.mainproject.group018.somojeon.candidate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+@RestController
+@RequestMapping("/candidates")
 public class CandidateController {
 
     @PostMapping
