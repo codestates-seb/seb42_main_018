2757cbba4a9fc49708068ea1330385afbd6785a9
Ahae KIM <kimaae@gmail.com>
2023-03-10 09:22:23 +0900

    🔥remove : conflict 해결 위해 gradle.yml 삭제

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/HelloController.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 class HelloController{
     @GetMapping
     public String hello(){
-        return "hello";
+        return "hello, team 18!";
     }
 }
