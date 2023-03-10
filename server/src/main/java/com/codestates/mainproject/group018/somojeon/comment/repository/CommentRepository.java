8897f616a78ed37870cfac66ecdf40bb6f29a284
Daewoon <pdw4904@gmail.com>
2023-03-10 13:06:01 +0900

    ♻️ refactor : "Category/Club 컨트롤러 수정"

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.mainproject.group018.somojeon.comment.repository;
-
-import com.codestates.mainproject.group018.somojeon.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
