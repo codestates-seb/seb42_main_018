bca35d372b3afd062940a9b9061bd948fb30b8f5
taekie <euntaek0813@naver.com>
2023-03-13 13:31:18 +0900

    ♻️ refactor : image, S3service update

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/images/service/S3Service.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/images/service/S3Service.java
@@ -2,66 +2,69 @@ package com.codestates.mainproject.group018.somojeon.images.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.codestates.mainproject.group018.somojeon.exception.BusinessLogicException;
-import com.codestates.mainproject.group018.somojeon.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
+@Component
+@Slf4j
 @RequiredArgsConstructor
-public class S3Service implements UploadService {
+public class S3Service {
 
     private final AmazonS3Client amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String upload(MultipartFile multipartFile) {
-        String fileName = createFileName(multipartFile.getOriginalFilename());
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(multipartFile.getSize());
-        objectMetadata.setContentType(multipartFile.getContentType());
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("Multipartfile -> File 전환 실패"));
+        return upload(uploadFile, dirName);
+    }
 
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            amazonS3Client.putObject(
-                    new PutObjectRequest(bucket, fileName, inputStream, objectMetadata).withCannedAcl(
-                            CannedAccessControlList.PublicRead
-                    )
-            );
-        } catch (IOException e) {
-            throw new IllegalArgumentException(String.format("파일 변환 중 에러가 발행하였습니다 (%s)", multipartFile.getOriginalFilename()));
-        }
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+
+        removeNewFile(uploadFile);
+
+        return uploadImageUrl;
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
-    @Override
-    public void delete(String fileName) {
-        String imageUrl = fileName.split("/")[3];
-
-        if (!amazonS3Client.doesObjectExist(bucket, imageUrl)) {
-            throw new BusinessLogicException(ExceptionCode.NOT_FOUND_EXCEPTION);
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제 되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
         }
-        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, imageUrl));
     }
 
-    private String createFileName(String fileName) {
-        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
-    }
-
-    private String getFileExtension(String fileName) {
-        try {
-            return fileName.substring(fileName.lastIndexOf("."));
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
         }
+        return Optional.empty();
     }
 }
--- server/src/main/java/com/codestates/mainproject/group018/somojeon/images/service/UploadService.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.mainproject.group018.somojeon.images.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadService {
-    String upload(MultipartFile multipartFile);
-    void delete(String fileName);
-}
