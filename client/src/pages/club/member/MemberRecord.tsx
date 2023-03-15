5ce01973ae269931219bee9c3315f2593af28734
therefrom <therefrom@naver.com>
2023-03-15 18:03:24 +0900

    [FE]✨feat: 소모임 멤버 목록

--- client/src/pages/club/member/MemberRecord.tsx
+++ client/src/pages/club/member/MemberRecord.tsx
@@ -1,13 +1,25 @@
+import styled from "styled-components";
 import Tabmenu from "../../../components/TabMenu";
 import S_Container from "../../../components/UI/S_Container";
 import MemberNav from "./MemberNav";
+import MemberRecordList from "./MemberRecordList";
+import MemberRecordTitle from "./MemberRecordTitle";
+
+const S_Table = styled.div`
+`
 
 function MemberRecord() {
     return (
         <S_Container>
             <Tabmenu />
             <MemberNav />
-            소모임 멤버 기록 탭
+            <S_Table>
+                <MemberRecordTitle/>
+                <tbody>
+                    <MemberRecordList/>
+                    <MemberRecordList/>
+                </tbody>
+            </S_Table>
         </S_Container>
     )
 }
--- client/src/pages/club/member/MemberRecordList.tsx
+++ client/src/pages/club/member/MemberRecordList.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import styled from "styled-components";
+
+interface Contents {
+  name: string;
+  width?: string;
+  color?: string;
+}
+
+const mainContents: Contents[] = [
+  {name: '1'},
+  {name: '벌꿀오소리'},
+  {name: '58%', color: 'var(--blue300)'},
+  {name: '12'},
+  {name: '10'},
+  {name: '2'},
+  {name: '0'},
+]
+
+const S_Td = styled.td<{ width?:string, color?:string }>`
+  padding: 20px 0px;
+  text-align: center;
+  
+  border-left: 1px solid var(--gray600);
+  border-top: 1px solid var(--gray100);
+  width: ${({ width }) => (width || '50px')};
+  color: ${({ color }) => (color)};
+`
 
 function MemberRecordList() {
   return (
-    <div>MemberRecordList</div>
+    <tr>
+      {mainContents.map((e)=>(
+        <S_Td key={e.name} width={e.width} color={e.color}>{e.name}</S_Td>
+      ))}
+    </tr>
   )
 }
 
