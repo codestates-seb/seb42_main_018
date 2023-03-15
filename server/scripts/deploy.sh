1d52fcbac3e89e8c33f7187540206d0d0c4f5659
gnswp21 <gnswp21@gmail.com>
2023-03-16 00:28:01 +0900

    🚑 !HOTFIX dev 자동배포

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -26,4 +26,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=server --server.port=8081>> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+nohup java -jar "somojeon-0.0.1-SNAPSHOT.jar" --spring.profiles.active=server --server.port=8081>> /home/ubuntu/action/server.log
