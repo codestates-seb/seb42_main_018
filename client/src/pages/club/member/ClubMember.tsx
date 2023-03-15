15ef34ba95ab88af33bfbb91660b79b94739b7e0
therefrom <therefrom@naver.com>
2023-03-15 14:16:50 +0900

    [FE] ✨feat: 소모임 소개/경기정보/멤버 탭메뉴 생성

--- client/src/pages/club/member/ClubMember.tsx
+++ client/src/pages/club/member/ClubMember.tsx
@@ -1,8 +1,12 @@
+import Tabmenu from "../../../components/TabMenu";
+import S_Container from "../../../components/UI/S_Container";
+
 function ClubMember() {
     return (
-        <div>
+        <S_Container>
+            <Tabmenu />
             소모임 멤버 탭 화면
-        </div>
+        </S_Container>
     )
 }
 
