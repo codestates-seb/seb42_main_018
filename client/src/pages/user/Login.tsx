50e0ba7052dbd06d1b132037e04b56b6189a163b
Ahae KIM <kimaae@gmail.com>
2023-03-16 13:57:15 +0900

    ♻️refactor : 공통 버튼 컴포 style 속성, onClick 등 props 구현

--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -1,9 +1,21 @@
+import S_Container from '../../components/UI/S_Container';
+import { S_Button } from '../../components/UI/S_Button';
 function Login() {
-    return (
-        <div>
-            로그인페이지
-        </div>
-    )
+  const handleKakaoLogin = () => {
+    console.log('카카오 로그인 클릭');
+  };
+
+  return (
+    <S_Container>
+      로그인페이지
+      <S_Button
+        onClick={handleKakaoLogin}
+        addStyle={{ backgroundColor: '#FAE64C', color: 'var(--gray500)' }}
+      >
+        카카오톡 로그인
+      </S_Button>
+    </S_Container>
+  );
 }
 
 export default Login;
