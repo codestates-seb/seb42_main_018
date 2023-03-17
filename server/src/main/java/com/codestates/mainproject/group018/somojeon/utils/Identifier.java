9da9da467ca89337e0cf1c40ba74725c211bd11e
gnswp21 <gnswp21@gmail.com>
2023-03-17 16:38:20 +0900

    ♻️ refactor identifier 수정

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/utils/Identifier.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/utils/Identifier.java
@@ -1,5 +1,6 @@
 package com.codestates.mainproject.group018.somojeon.utils;
 
+import com.codestates.mainproject.group018.somojeon.auth.service.AuthService;
 import com.codestates.mainproject.group018.somojeon.auth.token.CustomAuthenticationToken;
 import com.codestates.mainproject.group018.somojeon.club.enums.ClubRole;
 import com.codestates.mainproject.group018.somojeon.club.service.ClubService;
@@ -9,31 +10,37 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
 public class Identifier {
     private final UserService userService;
     private final ClubService clubService;
+
+    private final AuthService authService;
     private String[] DEFAULT_ALLOWED_CLUBROLES = new String[]{"MANAGER", "LEADER"};
 
-    public Identifier(UserService userService, ClubService clubService) {
+    public Identifier(UserService userService, ClubService clubService, AuthService authService) {
         this.userService = userService;
         this.clubService = clubService;
+        this.authService = authService;
     }
 
-    public boolean checkVerified(Long targetId){
+    public boolean isVerified(Long targetId){
         // admin이거나 본인이 본인을 타겟으로 하면 true를 리턴
-        if(checkAdmin()) return true;
+        if(isAdmin()) return true;
         return getUserId() == targetId;
     }
 
-    public boolean checkAdmin(){
+    public boolean isAdmin(){
         List<String> currentUserRole = getRoles();
         return currentUserRole.contains("ROLE_ADMIN");
     }
+
+
 
     public Long getUserId(){
         CustomAuthenticationToken customAuthenticationToken = (CustomAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
@@ -62,6 +69,17 @@ public class Identifier {
         // 입력받은 아이디와 역할에 대해, 현재 유저가 조건을 만족하는지 검사
         ClubRole clubRole = clubService.getUserClub(getUserId(), clubId);
         return List.of(DEFAULT_ALLOWED_CLUBROLES).contains(clubRole.getRoles());
+    }
+
+    public String getAccessToken(HttpServletRequest request) {
+        String token = request.getHeader("Access");
+        if(token == null) return null;
+        // check verified access token
+        Map<String, Object> claims = authService.getClaimsValues(token);
+        String registration =  (String) claims.get("registration");
+        String registrationId =  (String) claims.get("registrationId");
+        if(registration != null && registrationId != null) return null;
+        return token;
     }
 
 }
