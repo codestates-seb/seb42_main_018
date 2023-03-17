aa6b704aac4dec96c85370452782034f3f476b68
Ahae KIM <kimaae@gmail.com>
2023-03-17 18:30:16 +0900

    ✨ feat : 로그인 페이지 구현

--- client/src/pages/user/Login.tsx
+++ client/src/pages/user/Login.tsx
@@ -1,31 +1,25 @@
-import axios from 'axios';
+import { useState } from 'react';
 import styled from 'styled-components';
+import { postFetch } from '../../util/api';
+import { checkEmail, checkPassword } from '../../util/authorization/checkPassword';
+import alertPreparingService from '../../util/alertPreparingService';
 import S_Container from '../../components/UI/S_Container';
 import { S_Button, S_EditButton } from '../../components/UI/S_Button';
 import { S_Title, S_Label, S_Description } from '../../components/UI/S_Text';
 import { S_Input } from '../../components/UI/S_Input';
-import { postFetch } from '../../API_TEST';
 
 const S_LoginWrapper = styled.div`
   height: calc(90vh - 50px);
   display: flex;
   flex-direction: column;
-  /* background-color: yellowgreen; */
 
   & > .title-wrapper {
-    /* background-color: yellowgreen; */
     height: 20vh;
     display: flex;
     justify-content: center;
     align-items: center;
   }
-
-  & > .form-wrapper {
-    /* background-color: lightcoral; */
-  }
-
   & > .oauth-wrapper {
-    /* background-color: lightgrey; */
     margin-top: auto;
     height: 11vh;
     display: flex;
@@ -45,29 +39,56 @@ const S_SearchPasswordWrapper = styled.div`
 `;
 
 function Login() {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: ''
+  });
+  const { email, password } = inputs;
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+  // console.log(inputs);
+
   // * 카카오톡 로그인 관련 로직
-  const KAKAO_LOGIN_URL = 'https://dev.somojeon.site/oauth2/authorization/kakao';
+  const KAKAO_LOGIN_URL = `${process.env.REACT_APP_URL}/oauth2/authorization/kakao`;
   const handleKakaoLogin = async () => {
     return window.location.assign(KAKAO_LOGIN_URL);
   };
 
-  // * 네이버 로그인 관련 로직
-  const handleNaverLogin = () => {
-    alert('준비 중인 서비스입니다.');
-  };
-
   // * POST 요청 관련 로직
-  const POST_URL = 'https://dev.somojeon.site/auth/login';
-  const newData = {
-    email: 'test4@gmail.com',
-    password: '1234'
-  };
+  const POST_URL = `${process.env.REACT_APP_URL}/auth/login`;
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const res = await postFetch(POST_URL, newData);
+
+    if (!email || !password) return;
+
+    // 이메일 & 비밀번호 유효성 검사
+    const isValidEmail = checkEmail(email);
+    const isValidPassword = checkPassword(password);
+    console.log(isValidEmail);
+
+    if (!isValidEmail) setEmailError(true);
+    else setEmailError(false);
+
+    if (!isValidPassword) setPasswordError(true);
+    else setPasswordError(false);
+
+    if (!isValidEmail || !isValidPassword) return;
+
+    // 서버에 post 요청
+    const res = await postFetch(POST_URL, inputs);
     console.log('응답 데이터: ', res);
+
+    // TODO : 응답 상태에 따라 리다이렉트 로직
   };
+
+  // TODO : 소모전 로그인하기 섹션 클릭하면 리다이렉트
 
   return (
     <S_Container>
@@ -86,9 +107,16 @@ function Login() {
                 name='email'
                 type='text'
                 width='96%'
-                // value={}
-                // onChange={onChange}
+                value={email}
+                onChange={onChange}
               />
+              {emailError && (
+                <>
+                  <S_Description color={'var(--red100)'}>
+                    유효하지 않은 형식의 이메일입니다.
+                  </S_Description>
+                </>
+              )}
             </div>
 
             <div>
@@ -100,18 +128,22 @@ function Login() {
                 name='password'
                 type='text'
                 width='96%'
-                // value={}
-                // onChange={onChange}
+                value={password}
+                onChange={onChange}
               />
-              <S_Description color={'var(--red100)'}>
-                비밀번호는 영문 알파벳과 숫자를 최소 1개 이상 포함하여 8~20자여야 합니다.
-              </S_Description>
+              {passwordError && (
+                <>
+                  <S_Description color={'var(--red100)'}>
+                    비밀번호는 영문 알파벳과 숫자를 최소 1개 이상 포함하여 8~20자여야 합니다.
+                  </S_Description>
+                </>
+              )}
             </div>
 
             <S_Button>로그인하기</S_Button>
           </form>
           <S_SearchPasswordWrapper>
-            <S_EditButton>비밀번호 찾기</S_EditButton>
+            <S_EditButton onClick={alertPreparingService}>비밀번호 찾기</S_EditButton>
             <S_Description>&middot;</S_Description>
             <S_EditButton>회원가입</S_EditButton>
           </S_SearchPasswordWrapper>
@@ -129,7 +161,7 @@ function Login() {
             카카오톡 로그인
           </S_Button>
           <S_Button
-            onClick={handleNaverLogin}
+            onClick={alertPreparingService}
             addStyle={{ backgroundColor: '#5AC466', hoverBgColor: '#33a45c' }}
           >
             네이버 로그인
