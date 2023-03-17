19b8e5833ec12e61a419a173f3e3e7fb9d92db6d
gnswp21 <gnswp21@gmail.com>
2023-03-17 11:36:23 +0900

    ✨ feat : auditable 클래스

---
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/audit/Auditable.java
@@ -0,0 +1,26 @@
+package com.codestates.mainproject.group018.somojeon.audit;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+    @CreatedDate
+    @Column(name = "CREATED_AT", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "MODIFIED_AT")
+    private LocalDateTime modifiedAt;
+}
