82005fa90934a8a4362e0dd4e1a2db6179250c79
Ahae KIM <kimaae@gmail.com>
2023-03-20 17:48:57 +0900

    🐛 fix : Tabmen 컴포에서 /:id 동적으로 처리할 수 있게 수정

--- client/src/components/TabMenu.tsx
+++ client/src/components/TabMenu.tsx
@@ -1,12 +1,12 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const S_TabContainer = styled.div`
   // 탭메뉴 전체 박스
   display: flex;
   margin: -20px -20px 20px -20px;
-`
-const S_TabItem= styled.button<{ active: boolean }>`
+`;
+const S_TabItem = styled.button<{ active: boolean }>`
   // 탭 하나하나 버튼 스타일
   flex-grow: 1;
   height: 50px;
@@ -15,12 +15,12 @@ const S_TabItem= styled.button<{ active: boolean }>`
   font-weight: 600;
   background-color: var(--white);
   color: ${({ active }) => (active ? 'var(--gray600)' : 'var(--gray300)')};
-  border-bottom: 2px solid ${({ active }) => (active ? 'var(--gray600)' : 'var(--white)')};;
-  
+  border-bottom: 2px solid ${({ active }) => (active ? 'var(--gray600)' : 'var(--white)')};
+
   :hover {
     color: var(--gray600);
   }
-`
+`;
 interface Tab {
   id: number;
   title: string;
@@ -29,24 +29,24 @@ interface Tab {
 
 function Tabmenu() {
   const navigate = useNavigate();
-  const location = useLocation().pathname
-  
+  const location = useLocation().pathname;
+  const { id } = useParams();
+
   const tabs: Tab[] = [
-    {id: 1, title: '소개', path: '/club/:id'},
-    {id: 2, title: '경기정보', path: '/club/:id/match'}, 
-    {id: 3, title: '멤버', path: '/club/:id/member'}, 
-  ]
+    { id: 1, title: '소개', path: `/club/${id}` },
+    { id: 2, title: '경기정보', path: `/club/${id}/match` },
+    { id: 3, title: '멤버', path: `/club/${id}/member` }
+  ];
 
   return (
     <S_TabContainer>
       {tabs.map((tab) => (
-        <S_TabItem 
-          key={tab.id}
-          active={location === tab.path}
-          onClick={() => navigate(tab.path)}>{tab.title}</S_TabItem>
+        <S_TabItem key={tab.id} active={location === tab.path} onClick={() => navigate(tab.path)}>
+          {tab.title}
+        </S_TabItem>
       ))}
     </S_TabContainer>
-  )
+  );
 }
 
 export default Tabmenu;
--- client/src/pages/club/intro/ClubIntro.tsx
+++ client/src/pages/club/intro/ClubIntro.tsx
@@ -1,9 +1,12 @@
+import S_Container from '../../../components/UI/S_Container';
+import Tabmenu from '../../../components/TabMenu';
 function ClubIntro() {
-    return (
-        <div>
-            소모임 소개 탭 화면
-        </div>
-    )
+  return (
+    <S_Container>
+      <Tabmenu></Tabmenu>
+      <p>소모임 소개 탭 화면</p>
+    </S_Container>
+  );
 }
 
 export default ClubIntro;
