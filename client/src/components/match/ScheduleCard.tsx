ed0ebbfda2462a237c011c8e1d2332d2c616fb0c
dev-jq1 <dev.jq1@gmail.com>
2023-03-20 17:16:02 +0900

    🐛 fix: scheduleCard 클릭한 버튼에 맞게 색상 변화 구현, clubSchedule 에서 S_TabButton 분리

--- client/src/components/match/ScheduleCard.tsx
+++ client/src/components/match/ScheduleCard.tsx
@@ -1,39 +1,59 @@
-import styled from "styled-components";
-import { S_SelectButton } from "../UI/S_Button";
-import { S_Label, S_Text } from "../UI/S_Text";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { S_SelectButton } from '../UI/S_Button';
+import { S_Label, S_Text } from '../UI/S_Text';
 
 const S_CardContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 const S_Information = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const S_ButtonContainer = styled.div`
-    display: flex;
-    align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
 
 function ScheduleCard() {
-    return (
-        <>
-        <hr style={{margin:"20px 0"}}/>
-        <S_CardContainer>
-            <S_Information>
-                <S_Text>3월 11일 목요일 17:00</S_Text>
-                <S_Label>도봉구 약수터 풋살장</S_Label>
-            </S_Information>
-            <S_ButtonContainer>
-                <S_SelectButton>참석</S_SelectButton>
-                <S_SelectButton>불참</S_SelectButton>
-            </S_ButtonContainer>
-        </S_CardContainer>
-        </>
-    )
+  const [clickedButton, setClickedButton] = useState<string | null>('');
+  const buttonHandler = (e: React.MouseEvent<HTMLElement>) => {
+    setClickedButton(e.currentTarget.getAttribute('name'));
+  };
+  return (
+    <>
+      <hr style={{ margin: '20px 0' }} />
+      <S_CardContainer>
+        <S_Information>
+          <S_Text>3월 11일 목요일 17:00</S_Text>
+          <S_Label>도봉구 약수터 풋살장</S_Label>
+        </S_Information>
+        <S_ButtonContainer>
+          <S_SelectButton
+            name='attendance'
+            clicked={`${clickedButton}`}
+            className={clickedButton === 'attendance' ? 'clicked' : ''}
+            onClick={buttonHandler}
+            style={{ margin: '2px' }}
+          >
+            참석
+          </S_SelectButton>
+          <S_SelectButton
+            name='absence'
+            clicked={`${clickedButton}`}
+            className={clickedButton === 'absence' ? 'clicked' : ''}
+            onClick={buttonHandler}
+            style={{ margin: '2px' }}
+          >
+            불참
+          </S_SelectButton>
+        </S_ButtonContainer>
+      </S_CardContainer>
+    </>
+  );
 }
 
 export default ScheduleCard;
