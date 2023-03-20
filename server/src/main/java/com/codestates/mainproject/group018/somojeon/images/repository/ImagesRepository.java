6e7b3ffc8d6c9f9cb19cb2a142b6e0472c7b0de9
taekie <euntaek0813@naver.com>
2023-03-20 15:10:52 +0900

    🔥 remove : images 재업로드 위해 삭제

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/images/repository/ImagesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.mainproject.group018.somojeon.images.repository;
-
-import com.codestates.mainproject.group018.somojeon.images.entity.Images;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImagesRepository extends JpaRepository<Images, Long> {
-}
