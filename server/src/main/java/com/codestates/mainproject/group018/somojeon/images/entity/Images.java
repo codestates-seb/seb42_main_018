3fea188f3ad71c5666abc741d5796bced7ab85fe
taekie <euntaek0813@naver.com>
2023-03-20 15:58:22 +0900

    ♻️ refactor : images update

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/images/entity/Images.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/images/entity/Images.java
@@ -2,9 +2,16 @@ package com.codestates.mainproject.group018.somojeon.images.entity;
 
 import com.codestates.mainproject.group018.somojeon.club.entity.Club;
 import com.codestates.mainproject.group018.somojeon.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Images {
 
     @Id
@@ -25,3 +32,4 @@ public class Images {
     @JoinColumn(name = "USER_ID")
     private User user;
 }
+
