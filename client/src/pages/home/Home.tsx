069a8aeebb4f220a2e0434cfcffac0b577a93fe4
Ahae KIM <kimaae@gmail.com>
2023-03-21 16:40:56 +0900

    ✅ test : 전역 상태 변수 확인

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,14 +1,19 @@
-import S_Container from "../../components/UI/S_Container";
-import MainContents from "../../components/home/_MainContents";
-import HomeIntro from "../../components/home/_HomeIntro";
+import getGlobalState from '../../util/authorization/getGlobalState';
+import S_Container from '../../components/UI/S_Container';
+import MainContents from '../../components/home/_MainContents';
+import HomeIntro from '../../components/home/_HomeIntro';
 
 function Home() {
-    return (
-        <S_Container>
-            <HomeIntro />
-            <MainContents />
-        </S_Container>
-    )
+  const { isLogin, userInfo, tokens } = getGlobalState();
+  console.log(isLogin);
+  console.log(userInfo);
+  console.log(tokens);
+  return (
+    <S_Container>
+      <HomeIntro />
+      <MainContents />
+    </S_Container>
+  );
 }
 
 export default Home;
