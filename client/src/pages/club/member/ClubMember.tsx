cb4310b588ea29cb2397d77ecc2aa76244ad28c1
Ahae KIM <kimaae@gmail.com>
2023-03-21 10:41:31 +0900

    ♻️refactor : TabMenu 컴포에 탭 구성 props 추가

--- client/src/pages/club/member/ClubMember.tsx
+++ client/src/pages/club/member/ClubMember.tsx
@@ -1,41 +1,49 @@
-import Tabmenu from "../../../components/TabMenu";
-import S_Container from "../../../components/UI/S_Container";
-import ClubMemberItem from "./_memberItem";
-import MemberNav from "./_memberNav";
+import { useParams } from 'react-router-dom';
+import Tabmenu from '../../../components/TabMenu';
+import S_Container from '../../../components/UI/S_Container';
+import ClubMemberItem from './_memberItem';
+import MemberNav from './_memberNav';
 
 export interface ClubMemberProps {
-    // 받아올 멤버 정보 타입 설정
-    memberId? : number;
-    profileImage: string;
-    name: string;
-    winRate: string;
+  // 받아올 멤버 정보 타입 설정
+  memberId?: number;
+  profileImage: string;
+  name: string;
+  winRate: string;
 }
 
 function ClubMember() {
-    // TODO: 하드코딩 데이터로 표시, 추후 axios get 요청 구현
-    const data:ClubMemberProps[] = [
-        {   
-            memberId : 1,
-            profileImage: '이미지',
-            name: '별명',
-            winRate: 'string',
-        }
-    ]
-    
-    return (
-        <S_Container>
-            <Tabmenu />
-            <MemberNav />
-            {data.map((e) => 
-                    <ClubMemberItem
-                        key={e.memberId}
-                        profileImage={e.profileImage}
-                        name={e.name}
-                        winRate={e.winRate}
-                    />
-                )}
-        </S_Container>
-    )
+  const { id } = useParams();
+  const tabs = [
+    { id: 1, title: '소개', path: `/club/${id}` },
+    { id: 2, title: '경기정보', path: `/club/${id}/match` },
+    { id: 3, title: '멤버', path: `/club/${id}/member` }
+  ];
+
+  // TODO: 하드코딩 데이터로 표시, 추후 axios get 요청 구현
+  const data: ClubMemberProps[] = [
+    {
+      memberId: 1,
+      profileImage: '이미지',
+      name: '별명',
+      winRate: 'string'
+    }
+  ];
+
+  return (
+    <S_Container>
+      <Tabmenu tabs={tabs} />
+      <MemberNav />
+      {data.map((e) => (
+        <ClubMemberItem
+          key={e.memberId}
+          profileImage={e.profileImage}
+          name={e.name}
+          winRate={e.winRate}
+        />
+      ))}
+    </S_Container>
+  );
 }
 
 export default ClubMember;
