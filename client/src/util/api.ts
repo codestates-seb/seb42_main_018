fc182ed1c68efe102ee072c6af17c673a58e624a
dev-jq1 <dev.jq1@gmail.com>
2023-03-21 10:53:10 +0900

    🐛 fix: postFetch 요청 data 에 따라 type지정을 위한 제네릭 추가 변경

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -10,7 +10,7 @@ export const getFetch = async (url: string) => {
 };
 
 // ? data의 타입 여러 가지인데 이 경우 타입 지정 어떻게 해결?
-export const postFetch = async (url: string, newData: any) => {
+export const postFetch = async <T>(url: string, newData: T) => {
   try {
     const res = await axios.post(url, JSON.stringify(newData), {
       headers: {
