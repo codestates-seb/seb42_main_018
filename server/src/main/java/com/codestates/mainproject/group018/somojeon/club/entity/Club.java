aa44c2646dea6972ebedea3e47f9df9e50bde1c9
Daewoon <pdw4904@gmail.com>
2023-03-21 17:35:57 +0900

    ♻️ refactor : "club엔티티 원복"

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/club/entity/Club.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/club/entity/Club.java
@@ -31,7 +31,7 @@ public class Club {
     @Column(nullable = false)
     private String clubName;
 
-    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(nullable = false)
