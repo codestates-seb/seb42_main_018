7acd8d6c41c5e4a521831c31f523177d8e013151
Ahae KIM <kimaae@gmail.com>
2023-03-22 13:53:23 +0900

    ✨ feat : 소모임 가입신청 api 테스트 완료

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,9 +2,13 @@ import S_Container from '../../components/UI/S_Container';
 import MainContents from '../../components/home/MainContents';
 import HomeIntro from '../../components/home/HomeIntro';
 import CreateClubImg from '../../components/home/CreateClubImg';
+import getGlobalState from '../../util/authorization/getGlobalState';
 
 function Home() {
-
+  const { isLogin, userInfo, tokens } = getGlobalState();
+  console.log(isLogin);
+  console.log(userInfo);
+  console.log(tokens);
   return (
     <S_Container>
       <HomeIntro />
