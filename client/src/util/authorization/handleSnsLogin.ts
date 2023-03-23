b9ed9ac834ea82b7265f5ba6d9e508407ea44471
Ahae KIM <kimaae@gmail.com>
2023-03-23 11:59:56 +0900

    ✅ test : returnurl query param 서버에 전달 가능한지 확인

--- client/src/util/authorization/handleSnsLogin.ts
+++ client/src/util/authorization/handleSnsLogin.ts
@@ -1,4 +1,4 @@
 export const handleKakaoLogin = async () => {
-  const KAKAO_LOGIN_URL = `${process.env.REACT_APP_URL}/oauth2/authorization/kakao`;
+  const KAKAO_LOGIN_URL = `${process.env.REACT_APP_URL}/oauth2/authorization/kakao?returnurl=/mypage`;
   return window.location.assign(KAKAO_LOGIN_URL);
 };
