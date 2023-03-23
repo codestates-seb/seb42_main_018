00235d264ea6d871a979e7befc157a70d72df1ae
therefrom <therefrom@naver.com>
2023-03-24 00:46:05 +0900

    🔧update: 정보수정 프로필 파일 첨부 미리보기 구현

--- client/src/pages/mypage/EditProfile.tsx
+++ client/src/pages/mypage/EditProfile.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Tabmenu from '../../components/TabMenu';
-import { S_Button, S_EditButton, S_SelectButton } from '../../components/UI/S_Button';
+import { S_Button, S_EditButton } from '../../components/UI/S_Button';
 import S_Container from '../../components/UI/S_Container';
 import { S_Label, S_Title } from '../../components/UI/S_Text';
 import getGlobalState from '../../util/authorization/getGlobalState';
 import styled from 'styled-components';
 import InputNickname from '../../components/login/_inputNickname';
-import { S_Input } from '../../components/UI/S_Input';
 
 const S_EditBox = styled.div`
   margin-top: 50px;
@@ -38,6 +37,7 @@ const S_ImgBox = styled.div`
 
 function EditProfile() {
   const navigate = useNavigate();
+
   const { userInfo } = getGlobalState();
   const [inputs, setInputs] = useState({
     nickName: userInfo.nickName
@@ -56,9 +56,24 @@ function EditProfile() {
   ];
 
   const uploadImg = () => {
+    // 버튼 클릭시 파일첨부(input=file 태그) 실행시켜주는 함수
     const inputname = document.getElementById('uploadImg');
     inputname?.click();
   };
+
+  const [imgFile, setImgFile] = useState(userInfo.profileImage);
+  // 기본적으로 보여지는 이미지 파일
+  function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    // 파일로 가져온 이미지 미리보기
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        setImgFile(reader.result as string);
+      });
+      reader.readAsDataURL(file);
+    }
+  }
 
   return (
     <S_Container>
@@ -69,10 +84,16 @@ function EditProfile() {
           <S_Label>프로필사진</S_Label>
           <S_ImgBox>
             {/* <img src={userInfo.profileImage} alt='프로필사진' /> */}
-            <img id='previewimg' src={userInfo.profileImage} alt='프로필사진' />
+            <img id='previewimg' src={imgFile ? imgFile : userInfo.profileImage} alt='프로필사진' />
             <label htmlFor='file'>
               <S_EditButton onClick={uploadImg}>변경</S_EditButton>
-              <input id='uploadImg' type='file' accept='image/*' hidden />
+              <input
+                id='uploadImg'
+                type='file'
+                accept='image/*'
+                onChange={handleFileUpload}
+                hidden
+              />
             </label>
           </S_ImgBox>
         </div>
