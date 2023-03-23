c619902a85624bae4e116ec86a0b30d120d882c2
taekie <euntaek0813@naver.com>
2023-03-23 13:25:46 +0900

    🔧 update : candidate dto 부분 수정

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/candidate/dto/CandidateDto.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/candidate/dto/CandidateDto.java
@@ -34,10 +34,8 @@ public class CandidateDto {
     @NoArgsConstructor
     public static class Response {
         private Long candidateId;
-        private Long userId;
         private String nickName;
         Candidate.Attendance attendance;
-        private Long scheduleId;
 
         public String getAttendance() {
             return attendance.getStatus();
