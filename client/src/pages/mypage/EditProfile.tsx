09f747fd15026ea6e812f7a3824f84b8cebbc19a
therefrom <therefrom@naver.com>
2023-03-24 00:06:46 +0900

    🔧update: 멤버목록 API 수정

--- client/src/pages/mypage/EditProfile.tsx
+++ client/src/pages/mypage/EditProfile.tsx
@@ -1,14 +1,52 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import DeleteAccount from './DeleteAccount';
 import Tabmenu from '../../components/TabMenu';
-import { S_Button } from '../../components/UI/S_Button';
+import { S_Button, S_EditButton, S_SelectButton } from '../../components/UI/S_Button';
 import S_Container from '../../components/UI/S_Container';
+import { S_Label, S_Title } from '../../components/UI/S_Text';
+import getGlobalState from '../../util/authorization/getGlobalState';
+import styled from 'styled-components';
+import InputNickname from '../../components/login/_inputNickname';
 import { S_Input } from '../../components/UI/S_Input';
-import { S_Description, S_Label, S_Text } from '../../components/UI/S_Text';
+
+const S_EditBox = styled.div`
+  margin-top: 50px;
+  h1 {
+    margin-bottom: 5vh;
+  }
+  .profileImgBox {
+    margin-bottom: 8vh;
+    img {
+      width: 80px;
+      height: 80px;
+      object-fit: cover;
+      border-radius: 10px;
+      margin-right: 10px;
+      background-color: var(--gray200);
+    }
+  }
+  .nicknamebox {
+    margin-bottom: 8vh;
+  }
+`;
+
+const S_ImgBox = styled.div`
+  // 유저 프로필이미지 설정
+  display: flex;
+  align-items: center;
+`;
 
 function EditProfile() {
   const navigate = useNavigate();
+  const { userInfo } = getGlobalState();
+  const [inputs, setInputs] = useState({
+    nickName: userInfo.nickName
+  });
+  const { nickName } = inputs;
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
 
   // 상단탭
   const tabs = [
@@ -17,25 +55,35 @@ function EditProfile() {
     { id: 3, title: '회원 탈퇴', path: `/mypage/edit/account` }
   ];
 
+  const uploadImg = () => {
+    const inputname = document.getElementById('uploadImg');
+    inputname?.click();
+  };
+
   return (
     <S_Container>
       <Tabmenu tabs={tabs} />
-      <div>
-        <S_Label>프로필사진</S_Label>
-        <S_Label>닉네임</S_Label>
-        <S_Description>닉네임은 언제든지 바꿀 수 있어요.</S_Description>
-        <S_Input></S_Input>
-        <S_Label>이메일</S_Label>
-        <S_Text>이메일 주소 고정</S_Text>
-        <S_Label>비밀번호</S_Label>
-        <S_Input></S_Input>
-        <S_Label>비밀번호 확인</S_Label>
-        <S_Input></S_Input>
-        <S_Button onClick={() => navigate('/mypage')}>회원정보 수정</S_Button>
-        <DeleteAccount />
-      </div>
+      <S_EditBox>
+        <S_Title>현재 프로필 수정</S_Title>
+        <div className='profileImgBox'>
+          <S_Label>프로필사진</S_Label>
+          <S_ImgBox>
+            {/* <img src={userInfo.profileImage} alt='프로필사진' /> */}
+            <img id='previewimg' src={userInfo.profileImage} alt='프로필사진' />
+            <label htmlFor='file'>
+              <S_EditButton onClick={uploadImg}>변경</S_EditButton>
+              <input id='uploadImg' type='file' accept='image/*' hidden />
+            </label>
+          </S_ImgBox>
+        </div>
+        <div className='nicknamebox'>
+          <InputNickname value={nickName} onChange={onChange} />
+        </div>
+      </S_EditBox>
+      <S_Button onClick={() => navigate('/mypage')}>회원정보 수정</S_Button>
     </S_Container>
   );
 }
 
 export default EditProfile;
+//
