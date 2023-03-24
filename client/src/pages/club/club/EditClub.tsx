925f652849c47e7b4085272fbef78f06eb2c0c84
Ahae KIM <kimaae@gmail.com>
2023-03-25 00:35:48 +0900

    ✨ feat : 소모임 프로필 사진 등록 기능 구현

--- client/src/pages/club/club/EditClub.tsx
+++ client/src/pages/club/club/EditClub.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import { getFetch, patchFetch } from '../../../util/api';
 import CreateLocal from './_createLocal';
 import CreateTag from './_createTag';
@@ -8,8 +9,9 @@ import { S_FormWrapper } from './CreateClub';
 import { S_Title, S_Label, S_Description } from '../../../components/UI/S_Text';
 import { S_Input } from '../../../components/UI/S_Input';
 import { S_TextArea } from '../../../components/UI/S_TextArea';
-import { S_Button } from '../../../components/UI/S_Button';
+import { S_Button, S_EditButton } from '../../../components/UI/S_Button';
 import { S_RadioWrapper } from './CreateClub';
+import { S_ImgBox } from '../../mypage/EditProfile';
 import { ClubData } from '../../../types';
 
 export interface EditClubDataType {
@@ -19,6 +21,30 @@ export interface EditClubDataType {
   tagName?: string[];
   isPrivate: boolean;
 }
+
+interface ImgFileType {
+  lastModified: number;
+  lastModifiedDate?: object;
+  name: string;
+  size: number;
+  type: string;
+  webkitRelativePath: string;
+}
+
+export const S_Label_mg_top = styled(S_Label)`
+  margin-top: 1rem;
+`;
+
+const S_ImageBox = styled(S_ImgBox)`
+  & > img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    border-radius: 10px;
+    margin-right: 10px;
+    background-color: var(--gray200);
+  }
+`;
 
 function EditClub() {
   const navigate = useNavigate();
@@ -33,7 +59,7 @@ function EditClub() {
     tagName: [],
     isPrivate: false
   });
-  const { categoryName, local: prevLocal, secret } = clubInfo || {};
+  const { categoryName, clubImageUrl, local: prevLocal, secret } = clubInfo || {};
   const { clubName, content } = inputs || {};
 
   useEffect(() => {
@@ -65,8 +91,6 @@ function EditClub() {
     getClubInfo();
   }, []);
 
-  // !TODO: profileImage patch 요청 어떻게 보낼지 BE와 방식 논의 필요
-
   const [tags, setTags] = useState<string[]>([]);
   const [localValue, setLocalValue] = useState('');
 
@@ -77,18 +101,46 @@ function EditClub() {
     setInputs({ ...inputs, [name]: name === 'isPrivate' ? value === 'true' : value });
   };
 
+  // !TODO: profileImage patch 요청 어떻게 보낼지 BE와 방식 논의 필요
+  // 버튼 클릭시 파일첨부(input#file) 실행시켜주는 함수. 못생긴 파일첨부 input은 안녕!
+  const uploadImg = () => {
+    const inputname = document.getElementById('uploadImg');
+    inputname?.click();
+  };
+
+  // 파일로 가져온 이미지 미리보기
+  const [imgFile, setImgFile] = useState(clubImageUrl);
+  // const [imgFile, setImgFile] = useState<ImgFileType>();
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    console.log(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        setImgFile(reader.result as string);
+      });
+      // setImgFile(file);
+      reader.readAsDataURL(file);
+    }
+  };
+
+  console.log(typeof imgFile); // string
+
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const URL = `${process.env.REACT_APP_URL}/clubs/${clubId}`;
     const updateData = {
       ...inputs,
       tagName: tags,
-      local: localValue
+      local: localValue,
+      // * 이미지 파일 string으로 추가
+      clubImageUrl: imgFile
     };
 
-    // console.log(updateData);
-    const res = await patchFetch(URL, updateData);
-    if (res) navigate(`/club/${clubId}`);
+    console.log(updateData);
+    // const res = await patchFetch(URL, updateData);
+    // if (res) navigate(`/club/${clubId}`);
   };
 
   return (
@@ -98,7 +150,7 @@ function EditClub() {
         <form onSubmit={onSubmit}>
           <div>
             <label htmlFor='clubName'>
-              <S_Label>소모임 이름 *</S_Label>
+              <S_Label_mg_top>소모임 이름 *</S_Label_mg_top>
             </label>
             <S_Input
               id='clubName'
@@ -112,7 +164,7 @@ function EditClub() {
           </div>
           <div>
             <label htmlFor='content'>
-              <S_Label>소모임 소개글 *</S_Label>
+              <S_Label_mg_top>소모임 소개글 *</S_Label_mg_top>
             </label>
             <S_TextArea
               id='content'
@@ -127,7 +179,7 @@ function EditClub() {
 
           <div>
             <label htmlFor='categoryName'>
-              <S_Label>카테고리</S_Label>
+              <S_Label_mg_top>카테고리</S_Label_mg_top>
             </label>
             <S_Description>소모임 종류는 처음 모임을 만든 다음에는 변경할 수 없어요.</S_Description>
             <S_Input
@@ -143,11 +195,28 @@ function EditClub() {
           <CreateTag tags={tags} setTags={setTags} />
 
           {/* //TODO: 사진 등록 영역 */}
-          <S_Label>사진 등록</S_Label>
+          <div>
+            <S_Label_mg_top>사진 등록</S_Label_mg_top>
+            <S_ImageBox>
+              <img id='previewimg' src={imgFile ? imgFile : clubImageUrl} alt='소모임 소개 사진' />
+              <label htmlFor='file'>
+                <S_EditButton type='button' onClick={uploadImg}>
+                  변경
+                </S_EditButton>
+                <input
+                  id='uploadImg'
+                  type='file'
+                  accept='image/*'
+                  onChange={handleFileUpload}
+                  hidden
+                />
+              </label>
+            </S_ImageBox>
+          </div>
 
           <fieldset className='isPrivate'>
             <div>
-              <S_Label>공개여부 *</S_Label>
+              <S_Label_mg_top>공개여부 *</S_Label_mg_top>
             </div>
             <S_RadioWrapper>
               <div className='partition'>
@@ -194,7 +263,9 @@ function EditClub() {
               </div>
             </S_RadioWrapper>
           </fieldset>
-          <S_Button>정보수정 완료</S_Button>
+          <div className='submit-btn-box'>
+            <S_Button>정보수정 완료</S_Button>
+          </div>
         </form>
       </S_FormWrapper>
     </S_Container>
