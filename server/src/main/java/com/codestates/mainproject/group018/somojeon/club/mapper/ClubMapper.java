2e4a4f32293fe9fa5a6367a5cc8f31c99e71ca2f
Daewoon <pdw4904@gmail.com>
2023-03-24 12:02:50 +0900

    🔧 update  :  "ClubMapper 수정 (로그인시 clubId 추가) "

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/club/mapper/ClubMapper.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/club/mapper/ClubMapper.java
@@ -9,6 +9,7 @@ import com.codestates.mainproject.group018.somojeon.tag.dto.TagDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -53,12 +54,34 @@ public interface ClubMapper {
                 .build();
     }
 
-    UserClubDto.Response userClubToUserCLubResponse(UserClub userClub);
+    default UserClubDto.Response userClubToUserCLubResponse(UserClub userClub) {
+        if ( userClub == null ) {
+            return null;
+        }
+
+        UserClubDto.Response.ResponseBuilder response = UserClubDto.Response.builder();
+
+        response.clubId(userClub.getClub().getClubId());
+        response.clubRole( userClub.getClubRole() );
+        response.level( userClub.getLevel() );
+        response.playCount( userClub.getPlayCount() );
+        response.winCount( userClub.getWinCount() );
+        response.winRate( (int) userClub.getWinRate() );
+
+        return response.build();
+    }
 
     default List<UserClubDto.Response> userClubsToUserCLubResponses(List<UserClub> userClubs){
-        return userClubs.stream()
-                .map(this::userClubToUserCLubResponse)
-                .collect(Collectors.toList());
-    };
+        if ( userClubs == null ) {
+            return null;
+        }
+
+        List<UserClubDto.Response> list = new ArrayList<>( userClubs.size() );
+        for ( UserClub userClub : userClubs ) {
+            list.add( userClubToUserCLubResponse(userClub));
+        }
+
+        return list;
+    }
 
 }
