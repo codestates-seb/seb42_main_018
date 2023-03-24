c5693e8f874c39316bd2948a874470f1d0c390f6
dev-jq1 <dev.jq1@gmail.com>
2023-03-24 17:44:14 +0900

    🔧 update: 경기일정조회 업데이트

--- client/src/components/match/ScheduleCard.tsx
+++ client/src/components/match/ScheduleCard.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { S_SelectButton } from '../UI/S_Button';
-import { S_Label, S_Text } from '../UI/S_Text';
+import { S_Label, S_SmallDescription, S_Text } from '../UI/S_Text';
 
 const S_CardContainer = styled.div`
   display: flex;
@@ -15,6 +15,12 @@ const S_Information = styled.div`
 `;
 
 const S_ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const S_ButtonWrapper = styled.div`
   display: flex;
   align-items: center;
 `;
@@ -33,33 +39,45 @@ function ScheduleCard(props: ScheduleCardProps) {
   };
   return (
     <>
-      <hr style={{ margin: '20px 0' }} />
       <S_CardContainer onClick={() => navigate(`/club/${props.clubId}/match/${props.scheduleId}`)}>
         <S_Information>
           <S_Text>{`${props.date} ${props.time}`}</S_Text>
           <S_Label>{props.placeName}</S_Label>
         </S_Information>
         <S_ButtonContainer>
-          <S_SelectButton
-            name='attendance'
-            clicked={`${clickedButton}`}
-            className={clickedButton === 'attendance' ? 'clicked' : ''}
-            onClick={buttonHandler}
-            style={{ margin: '2px' }}
+          <S_SmallDescription
+            onClick={(e) => {
+              e.stopPropagation();
+              navigate(`/club/${props.clubId}/match/${props.scheduleId}/edit`);
+            }}
+            color='var(--red100)'
+            style={{ textAlign: 'right', marginRight: '10px' }}
           >
-            참석
-          </S_SelectButton>
-          <S_SelectButton
-            name='absence'
-            clicked={`${clickedButton}`}
-            className={clickedButton === 'absence' ? 'clicked' : ''}
-            onClick={buttonHandler}
-            style={{ margin: '2px' }}
-          >
-            불참
-          </S_SelectButton>
+            수정
+          </S_SmallDescription>
+          <S_ButtonWrapper>
+            <S_SelectButton
+              name='attendance'
+              clicked={`${clickedButton}`}
+              className={clickedButton === 'attendance' ? 'clicked' : ''}
+              onClick={buttonHandler}
+              style={{ margin: '2px' }}
+            >
+              참석
+            </S_SelectButton>
+            <S_SelectButton
+              name='absence'
+              clicked={`${clickedButton}`}
+              className={clickedButton === 'absence' ? 'clicked' : ''}
+              onClick={buttonHandler}
+              style={{ margin: '2px' }}
+            >
+              불참
+            </S_SelectButton>
+          </S_ButtonWrapper>
         </S_ButtonContainer>
       </S_CardContainer>
+      <hr style={{ margin: '20px 0' }} />
     </>
   );
 }
