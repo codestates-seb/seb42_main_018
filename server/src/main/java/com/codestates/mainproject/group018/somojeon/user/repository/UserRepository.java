95d025e2d0a7c9cd69208039f81fc74d98ddb20c
taekie <euntaek0813@naver.com>
2023-03-27 01:05:53 +0900

    🔧 update : schedule service 부분 재수정

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/user/repository/UserRepository.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/user/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.codestates.mainproject.group018.somojeon.user.repository;
 
+import com.codestates.mainproject.group018.somojeon.candidate.entity.Candidate;
 import com.codestates.mainproject.group018.somojeon.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN u.candidateList cd WHERE cd.candidateId = :candidateId")
+    User findByCandidate(@Param("candidateId") long candidateId);
 }
