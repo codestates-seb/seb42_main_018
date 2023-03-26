36042e1a6ca0d35c010e293000f833a31e510c4d
Daewoon <pdw4904@gmail.com>
2023-03-26 17:19:19 +0900

    🔧 update : "엔티티 GenerationType 수정"

--- server/src/main/java/com/codestates/mainproject/group018/somojeon/club/entity/ClubTag.java
+++ server/src/main/java/com/codestates/mainproject/group018/somojeon/club/entity/ClubTag.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class ClubTag {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long clubTagId;
 
     @ManyToOne(fetch = FetchType.LAZY)
