5618e6afbcba2257df9e50ae6ea3a9a9d551bf07
Ahae KIM <kimaae@gmail.com>
2023-03-27 10:35:21 +0900

    ✨feat : access token 갱신 로직 구현

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -1,5 +1,45 @@
-import axios from 'axios';
-import { JwtTokensType } from '../store/store';
+import axios, { AxiosResponse } from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { JwtTokensType, setTokens } from '../store/store';
+
+const refreshAccessToken = async (res: AxiosResponse, tokens: JwtTokensType) => {
+  console.log('api.ts 파일', res);
+  console.log(res.headers['access-token-expired']);
+  console.log(typeof res.headers['access-token-expired']); // string
+
+  // * accessToken 만료 (maxAge: 30 min)
+  if (res.headers['Access-Token-Expired'] === 'True') {
+    const REFRESH_URL = `${process.env.REACT_APP_URL}/auth/refresh`;
+    const res = await axios.post(REFRESH_URL, '', {
+      headers: {
+        // 'Content-Type': 'application/json',
+        withCredentials: true,
+        Authorization: tokens && tokens.accessToken,
+        Refresh: tokens && tokens.refreshToken
+      }
+    });
+
+    // * refreshToken 만료 (maxAge: 420 min)
+    if (res.headers['Refresh-Token-Expired'] === 'True') {
+      const navigate = useNavigate();
+      navigate('/login');
+    }
+
+    // * refreshToken은 유효해서 accessToken이 새로 발급됨
+    if (res.status === 200) {
+      const dispatch = useDispatch();
+      dispatch(
+        setTokens({
+          accessToken: res.headers.authorization,
+          refreshToken: res.headers.refresh
+        })
+      );
+    }
+
+    // TODO: 원래 하려고 했던 요청 재시도
+  }
+};
 
 export const getFetch = async (url: string, tokens?: JwtTokensType) => {
   try {
@@ -11,7 +51,10 @@ export const getFetch = async (url: string, tokens?: JwtTokensType) => {
         Refresh: tokens && tokens.refreshToken
       }
     });
-    return res.data;
+
+    if (tokens) refreshAccessToken(res, tokens);
+
+    if (res.status === 200) return res.data;
   } catch (err) {
     console.error(err);
   }
@@ -27,6 +70,8 @@ export const postFetch = async <T>(url: string, newData: T, tokens?: JwtTokensTy
         Refresh: tokens && tokens.refreshToken
       }
     });
+
+    if (tokens) refreshAccessToken(res, tokens);
 
     if (res.status === 200 || res.status === 201) return res;
   } catch (err) {
@@ -50,6 +95,8 @@ export const patchFetch = async <T>(
       }
     });
 
+    if (tokens) refreshAccessToken(res, tokens);
+
     if (res.status === 200) return res;
   } catch (err) {
     console.error(err);
@@ -65,6 +112,9 @@ export const deleteFetch = async (url: string, tokens?: JwtTokensType) => {
         Refresh: tokens?.refreshToken
       }
     });
+
+    if (tokens) refreshAccessToken(res, tokens);
+
     if (res.status === 204) return res;
   } catch (err) {
     console.log(err);
