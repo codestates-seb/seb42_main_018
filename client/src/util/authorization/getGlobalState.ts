f977c36a7d3b37dc93fd0b1362f2cabb2b4076da
Ahae KIM <kimaae@gmail.com>
2023-03-29 15:11:47 +0900

    🔧update : 새로고침시 로그인 상태 유지

--- client/src/util/authorization/getGlobalState.ts
+++ client/src/util/authorization/getGlobalState.ts
@@ -2,9 +2,19 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 
 const getGlobalState = () => {
-  const isLogin = useSelector((state: RootState) => state.isLogin);
-  const userInfo = useSelector((state: RootState) => state.userInfo);
-  const tokens = useSelector((state: RootState) => state.tokens);
+  let isLogin = useSelector((state: RootState) => state.isLogin);
+  let userInfo = useSelector((state: RootState) => state.userInfo);
+  let tokens = useSelector((state: RootState) => state.tokens);
+
+  const sessionIsLogin = sessionStorage.getItem('isLogin');
+  const sessionUserInfo = sessionStorage.getItem('userInfo');
+  const sessionTokens = sessionStorage.getItem('tokens');
+
+  if (!userInfo.userId && sessionIsLogin && sessionUserInfo && sessionTokens) {
+    isLogin = JSON.parse(sessionIsLogin);
+    userInfo = JSON.parse(sessionUserInfo);
+    tokens = JSON.parse(sessionTokens);
+  }
 
   return { isLogin, userInfo, tokens };
 };
