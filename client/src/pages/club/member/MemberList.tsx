95105d9726bf8bdd3b2b189aaa13dd095364b4c6
therefrom <therefrom@naver.com>
2023-03-31 12:15:49 +0900

    🐛 fix: API 로딩 딜레이시 화면 수정

--- client/src/pages/club/member/MemberList.tsx
+++ client/src/pages/club/member/MemberList.tsx
@@ -13,23 +13,15 @@ function MemberList({ members }: ClubMemberProps) {
   return (
     <S_Box>
       {/* 받아온 데이터 맵핑 */}
-      {members.length === 0 ? (
-        <NoItem
-          src='https://3dicons.sgp1.cdn.digitaloceanspaces.com/v1/dynamic/color/notebook-dynamic-color.png'
-          alt='빈 상자 이미지'
-          label='아직 가입한 멤버가 없어요!'
+      {members.map((el) => (
+        <ClubMemberItem
+          key={el.userId}
+          profileImage={el.profileImage}
+          nickName={el.nickName}
+          winRate={el.winRate}
+          clubRole={el.clubRole}
         />
-      ) : (
-        members.map((el) => (
-          <ClubMemberItem
-            key={el.userId}
-            profileImage={el.profileImage}
-            nickName={el.nickName}
-            winRate={el.winRate}
-            clubRole={el.clubRole}
-          />
-        ))
-      )}
+      ))}
     </S_Box>
   );
 }
