195cfeea11a758d181debbd765bb45aac53bf4b6
Ahae KIM <kimaae@gmail.com>
2023-05-30 18:43:29 +0900

    🔧update: 전역 상태 저장 웹 스토리지 session에서 local로 수정

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import { persistReducer } from 'redux-persist';
-import storageSession from 'redux-persist/lib/storage/session';
+import storage from 'redux-persist/lib/storage';
 
 export interface RootState {
   isLogin: boolean;
@@ -90,7 +90,7 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
   key: 'root',
-  storage: storageSession
+  storage
   // whitelist: ['isLogin', 'userInfo', 'tokens']
 };
 
